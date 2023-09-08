ba74882cb3c8c4759a2442d95c20f8365bc726dd
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2023-09-08 09:25:43 +0300

    добавлены условия в задачу про числа + обработано исключение пустой строки

--- python(введение)/Cеминар 2/HomeTask.py
+++ python(введение)/Cеминар 2/HomeTask.py
@@ -17,6 +17,8 @@ def coin():
 
     except ValueError:
         return 'Вы ввели не число'
+    except KeyboardInterrupt:
+        return "Ведены не все данные"
 print(coin())
 
 def watermelon_for_tescha():
@@ -29,6 +31,8 @@ def watermelon_for_tescha():
         return f"Самый легкий арбуз весит {min(lst)} кг а самый большой {max(lst)}"
     except ValueError:
         return 'Вы ввели не число'
+    except KeyboardInterrupt:
+        return "Ведены не все данные"
 print(watermelon_for_tescha())
 
 def guess_numbers():
@@ -38,15 +42,18 @@ def guess_numbers():
         num2 = int(input("Введите второе число от 1 го до 1000  "))
         p =  int(input('Ведите произведение ваших чисел   '))
         s =  int(input("Введите сумму ваших чисел   "))
-        if 1<= num1 <= 1000 and 1<=num2 <= 1000: 
+        if 1<= num1 <= 1000 and 1<=num2 <= 1000 and num1 + num2 == s and num1 * num2 == p: 
             for num1 in range(1, min(s, p) + 1):
                 num2 = s - num1
                 if num1 * num2 == p:
                     return f'Вы загадали числа {num1}  и {num1}'
+                
         else:
-            return f'Ваши числа не подходят под условие от 1 го до 1000'
+            return f'Ваши числа не подходят под условие'
     except ValueError:
         return 'Вы ввели не число'
+    except KeyboardInterrupt:
+        return "Ведены не все данные"
 
 print(guess_numbers())
 
@@ -60,4 +67,6 @@ def pow_two():
         return f' Целые степени двойки от одного до введенного числа это  - {lst}'
     except ValueError:
         return 'Вы ввели не число'
+    except KeyboardInterrupt:
+        return "Ведены не все данные"
 print(pow_two())
