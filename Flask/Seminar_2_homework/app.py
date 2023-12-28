aab292071683f36517d5ce868563c2b0f2401010
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2023-12-28 11:43:32 +0300

    домашняя работа семинар 2

--- Flask/Seminar_2_homework/app.py
+++ Flask/Seminar_2_homework/app.py
@@ -18,37 +18,36 @@ def index():
     """
     Страница с формой ввода
     пользовательских данных
-    """
-    if request.method == "POST":
+    """                                         
+    if request.method == "POST":                             # Если метод запроса POST
         response = make_response("Cookies")
-        username = request.form.get("username")
+        username = request.form.get("username")              # Получаем username и email  из формы
         email = request.form.get("email")
-        if len(email) != 0 and len(username) != 0:
-            cookie_name = response.set_cookie("username", value="username", max_age=None)
-            cookie_mail = response.set_cookie("email", value="email", max_age=None)
-            session["usename"] = username
-            print(request.form)
-            print(response)
+        if len(email) != 0 and len(username) != 0:           # Проверяем что данные введены
+            response.set_cookie("username", value="username", max_age=None)    
+            response.set_cookie("email", value="email", max_age=None)        # Создаем cookie почты и username
+            session["usename"] = username 
+            session["email"]   = email                                # Создаем сессию пользователя с email и username
             return redirect(url_for("user", username=username))
-        return redirect(url_for("index"))
-    return render_template("index.html")
+        return redirect(url_for("index"))                           # переход на страницу пользователя
+    return render_template("index.html")       
 
 
 @app.route("/user/<string:username>/", methods=["GET", "POST"])
 def user(username, *args, **kwargs):
     """
     Старница приветствия ползователя
-    """
-  
-    if session:
-        if request.method == "POST":
+    """                                      
+    if session:                             # Проверка наличия сесии               
+        if request.method == "POST":                          # Если метод Post (кнопка выход)
             response = make_response("Cookie clear")
-            if request.cookies.get("sessionid") and request.cookies.get("session"):
-                response.set_cookie("username", "", expires=0)
-                response.set_cookie("email", "", expires=0)
+            if request.cookies.get("sessionid") and request.cookies.get("session"):   # Проверяем наличие cookie сессии
+                response.set_cookie("csrftoken", "", expires=0)       
+                response.set_cookie("sessionid", "", expires=0)         # Очищаем cookie сесиии
+                response.set_cookie("session",'',expires=0)
                 print('Cookie удалены')
-                session.clear()
-                return redirect(url_for("index"))
+                session.clear()                                   # Удалаяем сессию
+                return redirect(url_for("index"))               # Возвращаемся на стартовую страницу
             print(request.cookies)
             print(response)
             print("Не удалось очистить Cookie")
